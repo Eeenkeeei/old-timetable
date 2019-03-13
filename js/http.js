ae8d2a346ab37719e0e1c877c5dd0b24d315adf1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-13 09:37:43 +0000

    Deploy Eeenkeeei/timetable to github.com/Eeenkeeei/timetable.git:gh-pages

--- js/http.js
+++ js/http.js
@@ -1,34 +1,6 @@
-export class Http {
-    items = 'items';
-    resultFlag = '1';
+export default class Http {
     constructor(url) {
         this.url = url;
-    }
-
-    getAll() {
-        return fetch(this.url);
-    }
-
-    getRegFlag() {
-        return fetch(`${this.url}/resultFlag`, {
-        })
-            // .then(dataWrappedByPromise => dataWrappedByPromise.json())
-            // .then(data => {
-            //     // you can access your data here
-            //     this.resultFlag = data;
-            //     console.log(this.resultFlag)
-            // });
-    }
-
-
-    save(item) {
-        return fetch(`${this.url}/resultFlag`, {
-            body: JSON.stringify(item),
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
     }
 
     add (item) {
