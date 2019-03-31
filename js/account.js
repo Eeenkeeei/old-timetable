74aa16043c85f8ba082751de904504c7c63b2536
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 21:51:51 +0000

    Deploy Eeenkeeei/timetable to github.com/Eeenkeeei/timetable.git:gh-pages

--- js/account.js
+++ js/account.js
@@ -21,7 +21,6 @@ if (storage.getUserData === null) {
 usernameBarEl.textContent = storage.getUserData.data.username;
 connectAccount.updateData();
 
-// если в хранилище нет данных редирект на начальную
 if (storage.getUserData === null) {
     document.location.href = 'index.html'
 } else {
@@ -37,113 +36,11 @@ exitButtonEl.addEventListener('click', (evt)=>{
 
 const rootDivEl = document.querySelector('#accountWindow');
 rootDivEl.innerHTML = '';
-function renderAccount (container) {
-    container.innerHTML = `
-    <div class="row" id="accountData">
-                            <label class="col-sm-5 h4 text-muted account-label">Учетная запись</label>
-                            <div class="col-sm-4" >
-                                <label  class="account-label h6 ">ИМЯ ПОЛЬЗОВАТЕЛЯ</label>
-                                <p><label data-id="accountName" class="h5"></label></p>
-                            </div>
-                
-                            <label class="col-sm-5 account-label"><h4></h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6">EMAIL</label>
-                                <p><label data-id="accountEmail" class="h5"></label></p>
-                            </div>
-                            <label class="col-sm-5 account-label"><h4></h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6">ВОЗРАСТ</label>
-                                <p><label data-id="accountAge" class="h5"></label></p>
-                            </div>
-
-                            <label class="col-sm-5 account-label"><h4></h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6">МЕСТО УЧЕБЫ</label>
-                                <p><label data-id="accountUniversity" class="h5"></label></p>
-                            </div>
-
-                            <label class="col-sm-5 account-label"><h4></h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6">ПОЛ</label>
-                                <p><label data-id="accountGender" class="h5"></label></p>
-                            </div>
-                            <div class="col-sm-12">
-                            <hr>
-                            </div>
-                            <label class="col-sm-5 text-muted account-label" ><h4>Сервисы</h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6">СТАРТОВАЯ СТРАНИЦА</label>
-                                <p><label data-id="accountStartPage" class="h5"></label></p>
-                            </div>
-
-                            <label class="col-sm-5 account-label"><h4></h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6">РАСПИСАНИЕ ЗАНЯТИЙ</label>
-                                <p><label data-id="accountLessonsTimetable" class="h5"></label></p>
-                            </div>
-                             <div class="col-sm-12">
-                            <hr>
-                            </div>
-                            <label class="col-sm-5 text-muted account-label" id="securityLabel"><h4>Безопасность</h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6" style="cursor: pointer" id="changePassword">Изменить пароль</label>
-                                <p><label data-id="accountStartPage" class="h5"></label></p>
-                            </div>
-                            <div class="col-sm-12">
-                            <hr>
-                            </div>
-                            <label class="col-sm-5 text-muted account-label" id=""><h4>Поддержка</h4></label>
-                            <div class="col-sm-4">
-                                <label class="account-label h6" style="cursor: pointer">Задать вопрос в поддержку</label>
-                              
-                            </div>
-                        </div>
-                        
-    `;
-    const accountDataNameEl = document.querySelector('[data-id=accountName]');
-    accountDataNameEl.textContent = user.username;
-
-    const accountDataEmailEl = document.querySelector('[data-id=accountEmail]');
-    accountDataEmailEl.textContent = user.email;
-
-    const accountDataAgeEl = document.querySelector('[data-id=accountAge]');
-    accountDataAgeEl.textContent = user.age;
-
-    const accountDataUniversityEl = document.querySelector('[data-id=accountUniversity]');
-    accountDataUniversityEl.textContent = user.edu;
-
-    const accountDataGenderEl = document.querySelector('[data-id=accountGender]');
-    accountDataGenderEl.textContent = user.gender;
-
-    const accountDataStartPageEl = document.querySelector('[data-id=accountStartPage]');
-
-    if (user.startPage === "account.html"){
-        accountDataStartPageEl.textContent = "Аккаунт";
-    }
-
-    if (user.startPage === "readlater.html"){
-        accountDataStartPageEl.textContent = "Список для чтения";
-    }
-
-    if (user.startPage === "tasktracker.html"){
-        accountDataStartPageEl.textContent = "Менеджер задач";
-    }
-
-    if (user.startPage === "timetable.html"){
-        accountDataStartPageEl.textContent = "Расписание";
-    }
-    passwordChanger.changePassword();
-}
-
-renderAccount(rootDivEl);
 
 let gender = '';
 let startPage = 'index.html';
-const changeDataButtonEl = document.querySelector('#changeDataButton');
 
 const editAccountListener = (evt) => {
-    changeDataButtonEl.className = 'btn btn-outline-dark invisible';
     rootDivEl.innerHTML = `
     <form id="accountChangeForm">
                         <h3>Изменение информации об аккаунте</h3>
@@ -316,12 +213,124 @@ const editAccountListener = (evt) => {
             accountChangeFormEl.appendChild(msgEl);
         }
         renderAccount(rootDivEl);
-        changeDataButtonEl.className = 'btn btn-outline-dark';
-
     });
 };
 
-changeDataButtonEl.addEventListener('click', editAccountListener);
+
+function renderAccount (container) {
+    container.innerHTML = `
+    <div class="row" id="accountData">
+                            <label class="col-sm-5 h4 text-muted account-label">Учетная запись</label>
+                            <div class="col-sm-4" >
+                                <label  class="account-label h6 ">ИМЯ ПОЛЬЗОВАТЕЛЯ</label>
+                                <p><label data-id="accountName" class="h5"></label></p>
+                            </div>
+                
+                            <label class="col-sm-5 account-label"><h4></h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6">EMAIL</label>
+                                <p><label data-id="accountEmail" class="h5"></label></p>
+                            </div>
+                            <label class="col-sm-5 account-label"><h4></h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6">ВОЗРАСТ</label>
+                                <p><label data-id="accountAge" class="h5"></label></p>
+                            </div>
+
+                            <label class="col-sm-5 account-label"><h4></h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6">МЕСТО УЧЕБЫ</label>
+                                <p><label data-id="accountUniversity" class="h5"></label></p>
+                            </div>
+
+                            <label class="col-sm-5 account-label"><h4></h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6">ПОЛ</label>
+                                <p><label data-id="accountGender" class="h5"></label></p>
+                            </div>
+                            <label class="col-sm-5 h6 account-label" ></label>
+                            <div class="col-sm-6" style="margin-top: 20px">
+                                <label  class="account-label h6 text-muted" id="changeDataButton" style="margin: 0px; cursor: pointer">РЕДАКТИРОВАТЬ УЧЕТНУЮ ЗАПИСЬ</label>
+                            </div>
+                            <div class="col-sm-12">
+                            <hr>
+                            </div>
+                            <label class="col-sm-5 text-muted account-label" ><h4>Сервисы</h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6">СТАРТОВАЯ СТРАНИЦА</label>
+                                <p><label data-id="accountStartPage" class="h5"></label></p>
+                            </div>
+
+                            <label class="col-sm-5 account-label"><h4></h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6">РАСПИСАНИЕ ЗАНЯТИЙ</label>
+                                <p><label data-id="accountLessonsTimetable" class="h5"></label></p>
+                            </div>
+                            
+                             <div class="col-sm-12">
+                            <hr>
+                            </div>
+                            <label class="col-sm-5 text-muted account-label" id="securityLabel"><h4>Безопасность</h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6 text-muted" style="cursor: pointer" id="changePassword">ИЗМЕНИТЬ ПАРОЛЬ</label>
+                                <p><label data-id="accountStartPage" class="h5"></label></p>
+                            </div>
+                            <div class="col-sm-12">
+                            <hr>
+                            </div>
+                            <label class="col-sm-5 text-muted account-label" id=""><h4>Поддержка</h4></label>
+                            <div class="col-sm-4">
+                                <label class="account-label h6 text-muted" style="cursor: pointer">ЗАДАТЬ ВОПРОС ПОДДЕРЖКЕ</label>
+                              
+                            </div>
+                        </div>
+                        
+    `;
+    const changeDataButtonEl = document.querySelector('#changeDataButton');
+
+    changeDataButtonEl.addEventListener('click', editAccountListener);
+    changeDataButtonEl.addEventListener('mouseout', ()=>{
+        changeDataButtonEl.classList.add('changeDataButtonNotFocus')
+    });
+    const accountDataNameEl = document.querySelector('[data-id=accountName]');
+    accountDataNameEl.textContent = user.username;
+
+    const accountDataEmailEl = document.querySelector('[data-id=accountEmail]');
+    accountDataEmailEl.textContent = user.email;
+
+    const accountDataAgeEl = document.querySelector('[data-id=accountAge]');
+    accountDataAgeEl.textContent = user.age;
+
+    const accountDataUniversityEl = document.querySelector('[data-id=accountUniversity]');
+    accountDataUniversityEl.textContent = user.edu;
+
+    const accountDataGenderEl = document.querySelector('[data-id=accountGender]');
+    accountDataGenderEl.textContent = user.gender;
+
+    const accountDataStartPageEl = document.querySelector('[data-id=accountStartPage]');
+
+    if (user.startPage === "account.html"){
+        accountDataStartPageEl.textContent = "Аккаунт";
+    }
+
+    if (user.startPage === "readlater.html"){
+        accountDataStartPageEl.textContent = "Список для чтения";
+    }
+
+    if (user.startPage === "tasktracker.html"){
+        accountDataStartPageEl.textContent = "Менеджер задач";
+    }
+
+    if (user.startPage === "timetable.html"){
+        accountDataStartPageEl.textContent = "Расписание";
+    }
+    passwordChanger.changePassword();
+}
+
+renderAccount(rootDivEl);
+
+
+
 
 
 const msgEl = document.createElement('div');
