b6c849cfcab56c25c6b34e545806312dd10015b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-03 20:05:33 +0000

    Deploy Eeenkeeei/timetable to github.com/Eeenkeeei/timetable.git:gh-pages

--- js/addLesson.js
+++ js/addLesson.js
@@ -5,7 +5,7 @@ import {Link} from "./lib.js";
 import Render from "./renderTimetable.js";
 import {ConnectAccount} from "./connectAccount.js";
 
-const http = new Http('https://timetable-eeenkeeei.herokuapp.com');
+const http = new Http('http://localhost:7777');
 // const authForSync = new WebSocket("ws://timetable-eeenkeeei.herokuapp.com/updateData");
 // const syncWithServer = new WebSocket("ws://timetable-eeenkeeei.herokuapp.com/sync");
 
@@ -166,7 +166,6 @@ addLessonButtonEl.addEventListener('click', () => {
             type: selectLessonType
         };
         user.timetable.push(lessonObject);
-        // console.log(user);
         const data = new Link(user);
         storage.add(data);
         let timetableUpdate = await http.timetableUpdate(user);
