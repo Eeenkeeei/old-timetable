e232fdcdac609fcb57fd9d80685488aaad13cf07
student <romanilichev@mail.ru>
2019-04-06 18:32:39 +0300

    изменение урла сервера

--- frontend/src/js/serverLink.js
+++ frontend/src/js/serverLink.js
@@ -1,6 +1,6 @@
 export class ServerLink{
     constructor() {
-        this.link = 'http://localhost:7777'
+        this.link = 'https://timetable-eeenkeeei.herokuapp.com'
         // https://timetable-eeenkeeei.herokuapp.com
         // http://localhost:7777
     }
