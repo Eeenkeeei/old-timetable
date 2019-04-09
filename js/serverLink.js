c2c2231b8588c9ba03adb89398c47cb965e9dea0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-09 17:14:58 +0000

    Deploy Eeenkeeei/timetable to github.com/Eeenkeeei/timetable.git:gh-pages

--- js/serverLink.js
+++
@@ -1,7 +0,0 @@
-export class ServerLink{
-    constructor() {
-        this.link = 'https://timetable-eeenkeeei.herokuapp.com'
-        // https://timetable-eeenkeeei.herokuapp.com
-        // http://localhost:7777
-    }
-}
